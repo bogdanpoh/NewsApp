3e22e7de0dec3b5e4d4372af6fc40c121bffc592
Bohdan Pokhidnia <bogdanpoh@icloud.com>
2022-07-16 22:43:33 +0300

    added in animator move author label, fixed bad show title label on transition

--- NewsApp/Sources/Animator.swift
+++ NewsApp/Sources/Animator.swift
@@ -18,30 +18,32 @@ final class Animator: NSObject, UIViewControllerAnimatedTransitioning {
         }
     }
     
-    static let duration: TimeInterval = 0.7
+    static let duration: TimeInterval = 0.6
 
     private let type: PresentationType
-    private let firstViewController: FeedViewController
-    private let secondViewController: DetailsViewController
+    private let fromViewController: FeedViewController
+    private let toViewController: DetailsViewController
+    private let window: UIWindow
     private var selectedCellImageViewSnapshot: UIView
-    private var cellImageViewRect: CGRect
-    private var cellWith: CGFloat = 382
+    
+    private let cellImageViewCoordinate: CGRect
+    private let cellLabelCoordinate: CGRect
+    private let cellAuthorLabelCoordinate: CGRect
 
-    private let cellLabelRect: CGRect
-
-    init?(type: PresentationType, firstViewController: FeedViewController, secondViewController: DetailsViewController, selectedCellImageViewSnapshot: UIView) {
+    init?(type: PresentationType, fromViewController: FeedViewController, toViewController: DetailsViewController, selectedCell: NewsCollectionViewCell) {
+        guard let window = fromViewController.contentView.window ?? toViewController.contentView.window,
+              let selectedCellImageViewSnapshot = selectedCell.articleImageView.snapshotView(afterScreenUpdates: true)
+        else { return nil }
+        
         self.type = type
-        self.firstViewController = firstViewController
-        self.secondViewController = secondViewController
+        self.fromViewController = fromViewController
+        self.toViewController = toViewController
+        self.window = window
         self.selectedCellImageViewSnapshot = selectedCellImageViewSnapshot
-
-        guard let window = firstViewController.contentView.window ?? secondViewController.contentView.window,
-            let selectedCell = firstViewController.selectedCell
-            else { return nil }
-
-        self.cellImageViewRect = selectedCell.articleImageView.convert(selectedCell.articleImageView.bounds, to: window)
-
-        self.cellLabelRect = selectedCell.titleLabel.convert(selectedCell.titleLabel.bounds, to: window)
+        
+        self.cellImageViewCoordinate = selectedCell.articleImageView.convert(selectedCell.articleImageView.bounds, to: window)
+        self.cellLabelCoordinate = selectedCell.titleLabel.convert(selectedCell.titleLabel.bounds, to: window)
+        self.cellAuthorLabelCoordinate = selectedCell.authorLabel.convert(selectedCell.authorLabel.bounds, to: window)
     }
 
     func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
@@ -49,96 +51,108 @@ final class Animator: NSObject, UIViewControllerAnimatedTransitioning {
     }
 
     func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
-        let containerView = transitionContext.containerView
-
-        guard let toView = secondViewController.view
-            else {
-                transitionContext.completeTransition(false)
-                return
+        guard let toView = toViewController.view else {
+            transitionContext.completeTransition(false)
+            return
         }
-
+        
+        let isPresenting = type.isPresenting
+        let containerView = transitionContext.containerView
         containerView.addSubview(toView)
-
-        guard let selectedCell = firstViewController.selectedCell,
-              let window = firstViewController.contentView.window ?? secondViewController.contentView.window,
+        
+        guard let selectedCell = fromViewController.selectedCell,
               let cellImageSnapshot = selectedCell.articleImageView.snapshotView(afterScreenUpdates: true),
-              let controllerImageSnapshot = secondViewController.contentView.articleImage.snapshotView(afterScreenUpdates: true),
-              let cellLabelSnapshot = selectedCell.titleLabel.snapshotView(afterScreenUpdates: true),
-              let closeButtonSnapshot = secondViewController.contentView.closeButton.snapshotView(afterScreenUpdates: true)
+              let controllerImageSnapshot = toViewController.contentView.articleImage.snapshotView(afterScreenUpdates: true),
+              var cellLabelSnapshot = selectedCell.titleLabel.snapshotView(afterScreenUpdates: true),
+              var cellAuthorLabelSnapshot = selectedCell.authorLabel.snapshotView(afterScreenUpdates: true),
+              let controllerCloseButtonSnapshot = toViewController.contentView.closeButton.snapshotView(afterScreenUpdates: true)
         else {
             transitionContext.completeTransition(true)
             return
         }
 
-        let isPresenting = type.isPresenting
-
         let backgroundView: UIView
         let fadeView = UIView(frame: containerView.bounds)
-        fadeView.backgroundColor = secondViewController.contentView.backgroundColor
+        fadeView.backgroundColor = toViewController.contentView.backgroundColor
 
         if isPresenting {
             selectedCellImageViewSnapshot = cellImageSnapshot
+            
+            if let presentedCellLabelSnapshot = toViewController.contentView.titleLabel.snapshotView(afterScreenUpdates: true) {
+                cellLabelSnapshot = presentedCellLabelSnapshot
+            }
+            
+            if let presentedCellAuthorLabelSnapshot = toViewController.contentView.authorLabel.snapshotView(afterScreenUpdates: true) {
+                cellAuthorLabelSnapshot = presentedCellAuthorLabelSnapshot
+            }
 
             backgroundView = UIView(frame: containerView.bounds)
-            backgroundView.addSubview(fadeView)
             fadeView.alpha = 0
         } else {
-            backgroundView = firstViewController.contentView.snapshotView(afterScreenUpdates: true) ?? fadeView
-            backgroundView.addSubview(fadeView)
+            backgroundView = fromViewController.contentView.snapshotView(afterScreenUpdates: true) ?? fadeView
         }
-
+        
+        backgroundView.addSubview(fadeView)
         toView.alpha = 0
+        
+        containerView.addSubviews(
+            backgroundView, selectedCellImageViewSnapshot, controllerImageSnapshot, cellLabelSnapshot, cellAuthorLabelSnapshot, controllerCloseButtonSnapshot
+        )
 
-        [backgroundView, selectedCellImageViewSnapshot, controllerImageSnapshot, cellLabelSnapshot, closeButtonSnapshot].forEach { containerView.addSubview($0) }
-
-
-        let controllerImageViewRect = secondViewController.contentView.articleImage.convert(secondViewController.contentView.articleImage.bounds, to: window)
-        let controllerLabelRect = secondViewController.contentView.titleLabel.convert(secondViewController.contentView.titleLabel.bounds, to: window)
-        let closeButtonRect = secondViewController.contentView.closeButton.convert(secondViewController.contentView.closeButton.bounds, to: window)
+        let controllerImageViewCoordinate = toViewController.contentView.articleImage.convert(toViewController.contentView.articleImage.bounds, to: window)
+        let controllerLabelCoordinate = toViewController.contentView.titleLabel.convert(toViewController.contentView.titleLabel.bounds, to: window)
+        let controllerAuthorLabelCoordinate = toViewController.contentView.authorLabel.convert(toViewController.contentView.authorLabel.bounds, to: window)
+        let controllerCloseButtonCoordinate = toViewController.contentView.closeButton.convert(toViewController.contentView.closeButton.bounds, to: window)
 
         [selectedCellImageViewSnapshot, controllerImageSnapshot].forEach {
-            $0.frame = isPresenting ? cellImageViewRect : controllerImageViewRect
+            $0.frame = isPresenting ? cellImageViewCoordinate : controllerImageViewCoordinate
 
-            $0.layer.cornerRadius = isPresenting ? 12 : 0
+            $0.layer.cornerRadius = isPresenting ? 15 : 0
             $0.layer.masksToBounds = true
         }
 
         controllerImageSnapshot.alpha = isPresenting ? 0 : 1
         selectedCellImageViewSnapshot.alpha = isPresenting ? 1 : 0
 
-        cellLabelSnapshot.frame = isPresenting ? cellLabelRect : controllerLabelRect
+        cellLabelSnapshot.frame = isPresenting ? cellLabelCoordinate : controllerLabelCoordinate
+        cellAuthorLabelSnapshot.frame = isPresenting ? cellAuthorLabelCoordinate : controllerAuthorLabelCoordinate
 
-        closeButtonSnapshot.frame = closeButtonRect
-        closeButtonSnapshot.alpha = isPresenting ? 0 : 1
+        controllerCloseButtonSnapshot.frame = controllerCloseButtonCoordinate
+        controllerCloseButtonSnapshot.alpha = isPresenting ? 0 : 1
 
         UIView.animateKeyframes(withDuration: Self.duration, delay: 0, options: .calculationModeCubic, animations: {
-            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.6) {
-                self.selectedCellImageViewSnapshot.frame = isPresenting ? controllerImageViewRect : self.cellImageViewRect
-                controllerImageSnapshot.frame = isPresenting ? controllerImageViewRect : self.cellImageViewRect
+            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.4) {
+                self.selectedCellImageViewSnapshot.frame = isPresenting ? controllerImageViewCoordinate : self.cellImageViewCoordinate
+                controllerImageSnapshot.frame = isPresenting ? controllerImageViewCoordinate : self.cellImageViewCoordinate
 
                 fadeView.alpha = isPresenting ? 1 : 0
-                cellLabelSnapshot.frame = isPresenting ? controllerLabelRect : self.cellLabelRect
+                cellLabelSnapshot.frame = isPresenting ? controllerLabelCoordinate : self.cellLabelCoordinate
+                
+                cellAuthorLabelSnapshot.frame = isPresenting ? controllerAuthorLabelCoordinate : self.cellAuthorLabelCoordinate
 
                 [controllerImageSnapshot, self.selectedCellImageViewSnapshot].forEach {
-                    $0.layer.cornerRadius = isPresenting ? 0 : 12
+                    $0.layer.cornerRadius = isPresenting ? 0 : 15
                 }
             }
 
-            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.6) {
+            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.4) {
                 self.selectedCellImageViewSnapshot.alpha = isPresenting ? 0 : 1
                 controllerImageSnapshot.alpha = isPresenting ? 1 : 0
             }
 
             UIView.addKeyframe(withRelativeStartTime: isPresenting ? 0.7 : 0, relativeDuration: 0.3) {
-                closeButtonSnapshot.alpha = isPresenting ? 1 : 0
+                controllerCloseButtonSnapshot.alpha = isPresenting ? 1 : 0
             }
-        }, completion: { isFinish in
-            self.selectedCellImageViewSnapshot.removeFromSuperview()
-            controllerImageSnapshot.removeFromSuperview()
-
-            backgroundView.removeFromSuperview()
-            cellLabelSnapshot.removeFromSuperview()
-            closeButtonSnapshot.removeFromSuperview()
+        }, completion: { (isFinish) in
+            [backgroundView,
+             self.selectedCellImageViewSnapshot,
+             controllerImageSnapshot,
+             cellLabelSnapshot,
+             cellAuthorLabelSnapshot,
+             controllerCloseButtonSnapshot
+            ].forEach {
+                $0.removeFromSuperview()
+            }
 
             toView.alpha = 1
 
